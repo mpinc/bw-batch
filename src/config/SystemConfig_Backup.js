040e52ce5134ed8864e12d36b62bb9de8f0a44c1
xueling795 <ling.xue@missionpublic.com>
2014-11-03 10:51:36 +0800

    update readme

--- src/config/SystemConfig_Backup.js
+++ src/config/SystemConfig_Backup.js
@@ -1,15 +1,20 @@
 /**
+ * Created by ibm on 14-11-3.
+ */
+
+/**
  * Created by ling xue on 14-5-29.
  */
 
 /*
-var mysqlConnectOptions ={
-    user: 'root',
-    password: 'Mission94539',
-    database:'bw',
-    host: 'bw.c9erfktsehne.us-west-1.rds.amazonaws.com'
-};
-*/
+ var mysqlConnectOptions ={
+ user: 'root',
+ password: 'Mission94539',
+ database:'bw',
+ host: 'bw.c9erfktsehne.us-west-1.rds.amazonaws.com'
+ };
+ */
+
 
 var mysqlConnectOptions ={
     user: 'biz',
